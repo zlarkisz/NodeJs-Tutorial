ee2871caf24fd992788b21afccfc7ebd5ccb7c1e
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-13 08:14:38 +0300

    lesson-2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello NodeJs');
+let x = 0
+
+if (x === 0) {
+  console.log('work');
+}
+
+for (let i = 0; i <= 5; i++) console.log(i);
+
+setTimeout(() => {
+  console.log('hello');
+}, 2000);
