62e6a9abec9e837f6fb41c24720124cd5ffd2ac2
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-15 14:19:07 +0300

    Node JS routing

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const fs = require('fs')
+
+// connect to local servre
 const http = require('http')
 
 const server = http.createServer((req, res) => {
   console.log(`Request url: ${req.url}`);
-  res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
 
-  const obj = {
-    model: 'Audi',
-    speed: '234.5',
-    wheels: 4
+  if (req.url === '/index' || req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    fs.createReadStream(__dirname + '/index.html').pipe(res)
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    fs.createReadStream(__dirname + '/about.html').pipe(res)
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+    fs.createReadStream(__dirname + '/404.html').pipe(res)
   }
-
-  res.end(JSON.stringify(obj))
-  // res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  // const myReadShort = fs.createReadStream(__dirname + '/index.html')
 })
 
 server.listen(3000, '192.168.0.102')
