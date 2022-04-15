8ae3e3416a7919547772945281aad7b441a1ef45
Gleb Bufin <zlarkisz@Zlarkisz.local>
2022-04-15 13:54:25 +0300

    The pipe() function, working with HTML and JSON

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const fs = require('fs')
+const http = require('http')
 
-const myReadShort = fs.createReadStream(__dirname + '/article.txt')
-let myWriteShort = fs.createWriteStream(__dirname + '/news.txt')
+const server = http.createServer((req, res) => {
+  console.log(`Request url: ${req.url}`);
+  res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
 
-myReadShort.on('data', (chunk) => {
-  console.log('Got new data');
+  const obj = {
+    model: 'Audi',
+    speed: '234.5',
+    wheels: 4
+  }
 
-  myWriteShort.write(chunk)
+  res.end(JSON.stringify(obj))
+  // res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  // const myReadShort = fs.createReadStream(__dirname + '/index.html')
 })
+
+server.listen(3000, '192.168.0.102')
+console.log('We are listening 3000 port');
